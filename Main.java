f50103dbcbc4f19d7668edb6d866f27a0c9942fd
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-08-23 11:13:09 -0300

    main method

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Tamagotchi pet = new Tamagotchi("Pippin");
+    pet.play();
+    pet.checkStatus();
+
+    // criar aqui com switch case um menu simples pra cada funcao
+  }
+}
